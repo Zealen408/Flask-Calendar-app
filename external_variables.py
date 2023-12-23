d3ae663225735332bc7eb0a2d4b5a3c36a17c630
Kristopher O <64108662+Zealen408@users.noreply.github.com>
2023-12-23 18:58:45 +0800

    Create external_variables.py

---
+++ external_variables.py
@@ -0,0 +1,33 @@
+conductors = [
+  'Barbara', 
+  'Charles', 
+  'Christopher', 
+  'Daniel', 
+  'David', 
+  'Elizabeth', 
+  'James', 
+  'Jennifer', 
+  'Jessica', 
+  'John', 
+  'Joseph', 
+  'Karen', 
+  'Linda', 
+  'Lisa', 
+  'Mary', 
+  'Michael', 
+  'Nancy', 
+  'Patricia', 
+  'Richard', 
+  'Robert', 
+  'Sarah', 
+  'Susan', 
+  'Thomas', 
+  'William'
+  ]
+
+locations = [
+  'Rome',
+  'Paris',
+  'London',
+  'Dubai',
+  ]
